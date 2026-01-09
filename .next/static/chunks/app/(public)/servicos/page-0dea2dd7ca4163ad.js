e02317821f2b061a58edd7f8681ec4c46dbe8f20
Shadow <gabrielandradenof+1@gmail.com>
2026-01-08 23:30:16 -0300

    fix: remove .next from git and update .gitignore to prevent Vercel build errors

--- .next/static/chunks/app/(public)/servicos/page-0dea2dd7ca4163ad.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5869,6465,8597],{78998:function(n,e,u){Promise.resolve().then(u.t.bind(u,72972,23))}},function(n){n.O(0,[2972,2971,2117,1744],function(){return n(n.s=78998)}),_N_E=n.O()}]);
